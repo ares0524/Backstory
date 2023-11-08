f72d79739c9a565f459ba386b12bd3d3f15f5eba
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-08 16:24:19 +0000

    Update _header.tsx
    - Moved library icon after search bar.
    - Changed 1000 tokens to 1k

--- app/components/layout/_header.tsx
+++ app/components/layout/_header.tsx
@@ -27,7 +27,6 @@ export default function Header({isLoggedIn}: HeaderProps) {
                 </div>
 
                 <div className={"flex items-center lg:order-2 " + (isLoggedIn?"block":"hidden")}>
-                    <a href="/library"><Image src="/assets/images/header/library.png" alt="Library" width={25} height={25} className="mr-4 cursor-pointer" /></a>
                     <form className="pr-4">   
                         <label htmlFor="default-search" className="mb-2 text-sm font-medium sr-only text-white">Search</label>
                         <div className="relative">
@@ -40,9 +39,10 @@ export default function Header({isLoggedIn}: HeaderProps) {
                             {/* <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button> */}
                         </div>
                     </form>
+                    <a href="/library"><Image src="/assets/images/header/library.png" alt="Library" width={25} height={25} className="mr-4 cursor-pointer" /></a>
                     <div className="flex pr-4">
                         <Image src="/assets/images/header/coin.png" alt="Coin" width={25} height={28} />
-                        <p className="text-xl items-center font-bold ml-2 text-white">1000</p>
+                        <p className="text-xl items-center font-bold ml-2 text-white">1k</p>
                     </div>
                     {/* <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
