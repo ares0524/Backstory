63c8b9d8b6f455cf0c5b62b6b59d9b7703a2f86f
ares0524 <snow520scene@gmail.com>
2023-11-10 05:38:09 +0900

    updated register form

--- app/components/layout/_header.tsx
+++ app/components/layout/_header.tsx
@@ -10,7 +10,7 @@ export default function Header({isLoggedIn}: HeaderProps) {
   return (
     <>
       <header>
-        <nav className="z-10 border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800 w-full fixed">
+        <nav className="z-10 border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800 w-full fixed top-0">
             <div className="flex flex-wrap justify-between items-center mx-auto">
                 <a href="#" className="flex items-center">
                     <Image src="/assets/images/logo.png" width={60} height={35} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,105 +1,154 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { register } from "../store/auth/AuthSlice";
+import { useDispatch } from "../store/store";
+import { useEffect } from "react";
 
-export default function Login() {
-    
+export default function Register() {
+    const dispatch = useDispatch();
 
-  return (
-    <>
-        <a href="#" className="flex items-center auth-logo">
-            <Image src="/assets/images/logo.png" width={60} height={35} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Backstory</span>
-        </a>
+    useEffect(() => {
+        const data = {
+            first_name: 'Cal',
+            last_name: 'West',
+            email: 'admin1@backstory.com',
+            password: 'rootroot'
+        }
+
+        dispatch(register(data));
+    })
+
+    const userRegister = () => {
         
-        <div className="flex flex-col items-center md:flex-row md:h-screen justify-center">
-            <div className="flex items-center justify-center w-full md:w-2/5">
-                <Image src="/assets/images/auth/register.png" alt="Register Image" width={500} height={350} />
-            </div>
-            <div className="flex flex-col items-center justify-center w-full md:w-1/4">
-                <div className="w-full max-w-md space-y-8">
-                <div>
-                    <h1 className="text-2xl font-bold">Welcome to the Backstory!</h1>
-                    <p className="mt-2 text-gray-600">
-                    Please register an account.
-                    </p>
+    }
+
+    return (
+        <>
+            <a href="#" className="flex items-center auth-logo">
+                <Image src="/assets/images/logo.png" width={60} height={35} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                <span className="self-center text-xl font-semibold whitespace-nowrap text-white">Backstory</span>
+            </a>
+            
+            <div className="flex flex-col items-center md:flex-row md:h-screen justify-center">
+                <div className="flex items-center justify-center w-full md:w-2/5">
+                    <Image src="/assets/images/auth/register.png" alt="Register Image" width={500} height={350} />
                 </div>
-                <form className="mt-8 space-y-6">
+                <div className="flex flex-col items-center justify-center w-full md:w-1/4">
+                    <div className="w-full max-w-md space-y-8">
                     <div>
-                    <label htmlFor="email" className="block font-bold text-[#dddddd]">
-                        Email address
-                    </label>
-                    <input
-                        id="email"
-                        type="email"
-                        placeholder="Enter your email"
-                        className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
-                        required
-                    />
+                        <h1 className="text-2xl font-bold">Welcome to the Backstory!</h1>
+                        <p className="mt-2 text-gray-600">
+                        Please register an account.
+                        </p>
                     </div>
-                    <div>
-                        <label
-                            htmlFor="password"
-                            className="block font-bold text-[#dddddd]"
-                        >
-                            Password
-                        </label>
-                        <input
-                            id="password"
-                            type="password"
-                            placeholder="Enter your password"
-                            className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
-                            required
-                        />
+                    <form className="mt-8 space-y-4">
+                        <div className="flex">
+                            <div className="w-1/2 pr-2">
+                                <label htmlFor="first_name" className="block font-bold text-[#dddddd]">
+                                    First Name
+                                </label>
+                                <input
+                                    id="first_name"
+                                    type="text"
+                                    placeholder="First Name"
+                                    className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+                                    required
+                                />
+                            </div>
+
+                            <div className="w-1/2 pl-2">
+                                <label htmlFor="last_name" className="block font-bold text-[#dddddd]">
+                                    Last Name
+                                </label>
+                                <input
+                                    id="last_name"
+                                    type="text"
+                                    placeholder="Last Name"
+                                    className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <label htmlFor="email" className="block font-bold text-[#dddddd]">
+                                Email address
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                placeholder="Enter your email"
+                                className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label
+                                htmlFor="password"
+                                className="block font-bold text-[#dddddd]"
+                            >
+                                Password
+                            </label>
+                            <input
+                                id="password"
+                                type="password"
+                                placeholder="Enter your password"
+                                className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label
+                                htmlFor="confirmPassword"
+                                className="block font-bold text-[#dddddd]"
+                            >
+                                Confirm Password
+                            </label>
+                            <input
+                                id="confirmPassword"
+                                type="password"
+                                placeholder="Confirm your password"
+                                className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
+                                required
+                            />
+                        </div>
+                    
+                        <div className="text-center">
+                            <Link
+                                href="/forget"
+                                className="text-md text-blue-600 hover:underline font-bold"
+                            >
+                                Terms and conditions
+                            </Link>
+                            <button
+                                type="submit"
+                                className="w-full px-4 py-3 font-bold text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700"
+                                onClick={() => userRegister()}
+                            >
+                                Register
+                            </button>
+                        </div>
+                    </form>
+
+                    <div className="relative flex items-center justify-center w-full mt-6 border border-t">
+                        <div className="absolute px-5 bg-[#2f2f2f]">Or</div>
                     </div>
 
-                    <div>
-                        <label
-                            htmlFor="confirmPassword"
-                            className="block font-bold text-[#dddddd]"
-                        >
-                            Confirm Password
-                        </label>
-                        <input
-                            id="confirmPassword"
-                            type="password"
-                            placeholder="Confirm your password"
-                            className="w-full px-4 py-3 mt-1 border-gray-300 rounded-md focus:border-indigo-500 focus:ring focus:ring-indigo-200"
-                            required
-                        />
-                    </div>
-                
-                    <div className="text-center">
+                    <p className="mt-4 text-sm text-center text-[#dddddd]">
+                        Have an account?{" "}
                         <Link
-                            href="/forget"
-                            className="text-md text-blue-600 hover:underline font-bold"
+                            href="/login"
+                            className="font-medium text-blue-600 hover:underline"
                         >
-                            Terms and conditions
+                            Login
                         </Link>
-                        <button
-                            type="submit"
-                            className="w-full px-4 py-3 font-bold text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700"
-                        >
-                            Register
-                        </button>
+                    </p>
                     </div>
-                </form>
-
-                <div className="relative flex items-center justify-center w-full mt-6 border border-t">
-                    <div className="absolute px-5 bg-[#2f2f2f]">Or</div>
-                </div>
-
-                <p className="mt-4 text-sm text-center text-[#dddddd]">
-                    Have an account?{" "}
-                    <Link
-                        href="/login"
-                        className="font-medium text-blue-600 hover:underline"
-                    >
-                        Login
-                    </Link>
-                </p>
                 </div>
             </div>
-        </div>
-    </>
-  );
+        </>
+    );
 }
