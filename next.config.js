9b19452c89ecdd9b303e3436ca945a54dee9416b
ares0524 <snow520scene@gmail.com>
2023-11-12 02:01:06 +0900

    fixed APIs

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
         NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: 'pk_test_51O7iZSKKxegAdYfWVGA8bfiXfcpnIt0cHVweX8SgFV28oPHVO8UVtaGKCGtPKpJ0fv1rF0QeP1hb583mbrfO9sde00lg3O3jHK',
         // MAIN_DOMAIN: 'http://localhost:3000',
         // SUB_DOMAIN: 'http://localhost:3001',
-        MAIN_DOMAIN: 'https://backstory-eta.vercel.app/',
-        SUB_DOMAIN: 'https://backstory-subdomain.vercel.app/',
+        MAIN_DOMAIN: 'https://backstory-eta.vercel.app',
+        SUB_DOMAIN: 'https://backstory-subdomain.vercel.app',
     }
 }
 
